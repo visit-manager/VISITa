ca0e4a68b43ecdc72a2285578675802e4326e463
Sachitama2001 <ononoimoko02@gmail.com>
2018-06-18 12:00:00 +0900

    snapshot(feature/new-function|visita_icdc10): code_visita_icdc10_20180618.tar

--- setting.h
+++ setting.h
@@ -37,8 +37,8 @@
 /* initial (minimal) carbon stock ***********/
 #define INT_C 0.01
 
-#define CALC_STEP 10
-#define CALC_OFFET 2
+#define CALC_STEP 1
+#define CALC_OFFET 0
 /* 1: every grid */
 /* 10: every 10 grid */
 
