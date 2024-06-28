fdafabee9382c4856f3b8e6a477e620dbf972ed2
Sachitama2001 <ononoimoko02@gmail.com>
2024-06-28 12:00:00 +0900

    snapshot(visita_test): code_visita_test_20240628.tar

--- setting.h
+++ setting.h
@@ -462,8 +462,8 @@
     /* #define DL_HCLIM 119 */  /* CRU TS4.04: AD 1901 - 2019 */
     /* #define DL_HCLIM 120 */  /* CRU TS4.05: AD 1901 - 2020 */
     /* #define DL_HCLIM 121 */  /* CRU TS4.06: AD 1901 - 2021 */
-    #define DL_HCLIM 122 /* */  /* CRU TS4.07: AD 1901 - 2022 */
-    /* #define DL_HCLIM 123 */  /* CRU TS4.08: AD 1901 - 2023 */
+    /* #define DL_HCLIM 122 */  /* CRU TS4.07: AD 1901 - 2022 */
+    #define DL_HCLIM 123 /* */  /* CRU TS4.08: AD 1901 - 2023 */
     /* 102: TS2.1 */
     /* 106: TS3.0 */
     /* 109: TS3.1 */
@@ -478,7 +478,7 @@
 #endif
 
 /* Simulation using NCEP/NCAR reanalysis data */
-#define NCEP_RUN 1
+#define NCEP_RUN 0
 /* 0: no  1:yes */
 /* year of data beginning (AD) */
 #define FDY_NCEP 1948
